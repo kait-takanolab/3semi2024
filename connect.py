4122b743e9fa1302d39a3ef4eb99a1858fc1e1c0
ktakano <takano@ic.kanagawa-it.ac.jp>
2024-06-05 12:08:03 +0900

    Added try - except handling.

--- connect.py
+++ connect.py
@@ -1,8 +1,12 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='root', password='',
+try:
+    cnx = mysql.connector.connect(user='', password='',
                               host='127.0.0.1',
                               database='3semi2024')
+except mysql.connector.Error as err:
+    print("Error: {}".format(err))
+    exit()
 
 print("MySQLに接続しました。")
 

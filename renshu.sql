be8217d76d1c3a3fb6e94fce6a77b7ea9ec7d413
ktakano <takano@ic.kanagawa-it.ac.jp>
2024-06-19 12:52:39 +0900

    Added topic and sentence table.

--- renshu.sql
+++ renshu.sql
@@ -4,3 +4,16 @@ CREATE TABLE student (
 );
 
 INSERT INTO student (student_id, name) VALUES ('2221000', '神奈川はなこ');
+
+CREATE TABLE topic (
+  id INT PRIMARY KEY,
+  title TEXT
+);
+
+CREATE TABLE sentence (
+  id INT PRIMARY KEY,
+  sentence TEXT,
+  audio_path TEXT,
+  topic_id INT,
+  FOREIGN KEY(topic_id) REFERENCES topic(id)
+);

77ce0041419f7f83d23ca506ecf938c5d89bba1c
ktakano <takano@ic.kanagawa-it.ac.jp>
2024-06-26 12:55:29 +0900

    Modify to split with '.'

--- text_to_speech.py
+++ text_to_speech.py
@@ -4,15 +4,23 @@ import os
 with open('en_text.txt') as f:
     lines = f.readlines()
 
-print (lines)
-#for i, line in enumerate(lines):
-#    line.replace('\n', '')
-#    print(line)
+allline = "".join(lines)
 
-line = lines
+print (allline)
+lines = allline.split('.')
+
+length = len(lines)
+print (lines, length)
+for i, line in enumerate(lines):
+    line.replace('\n', '')
+    print(line)
+    if line == '':
+        continue
+
+#line = lines[0]
 #line = "Hello, Sato-san."
-i = 0
+#i = 0
 
-tts = gTTS(text=line, lang='en')
-file_name = str(i) + ".mp3"
-tts.save(os.path.join("audio", file_name))
+    tts = gTTS(text=line, lang='en')
+    file_name = str(i) + ".mp3"
+    tts.save(os.path.join("audio", file_name))
